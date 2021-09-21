0bf9922ff67e0f97f52b2b916a58e0ab28998c62
baskadir <kadirbas09@gmail.com>
2021-09-21 10:33:51 +0300

    Created Custom Policies

---
+++ Data/CustomPolicies/CustomEmailPhonePolicy.cs
@@ -0,0 +1,48 @@
+﻿using Data.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.CustomPolicies
+{
+    public class CustomEmailPhonePolicy : UserValidator<User>
+    {
+        public async override Task<IdentityResult> ValidateAsync(UserManager<User> manager, User user)
+        {
+            IdentityResult result = await base.ValidateAsync(manager, user);
+            List<IdentityError> errors = result.Succeeded ? new List<IdentityError>() : result.Errors.ToList();
+
+            if (!user.Email.ToLower().EndsWith("@gmail.com"))
+            {
+                IdentityError error = new ()
+                {
+                    Code = "EmailValidation",
+                    Description = "Sadece gmail uzantılı eposta adresiyle kayıt olabilirsiniz."
+                };
+                errors.Add(error);
+            }
+            if (user.Email.Length>50)
+            {
+                IdentityError error = new()
+                {
+                    Code = "EmailLength",
+                    Description = "Email 50 karakterden fazla olamaz."
+                };
+                errors.Add(error);
+            }
+            if (user.PhoneNumber.Length != 11 && user.PhoneNumber.StartsWith("0"))
+            {
+                IdentityError error = new ()
+                {
+                    Code = "PhoneNumberValidation",
+                    Description = "Telefon numaranız yanlış" //mesajı sonra düzeltiriz
+                };
+                errors.Add(error);
+            }
+            return errors.Count == 0 ? IdentityResult.Success : IdentityResult.Failed(errors.ToArray());
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,6 +1,11 @@
+using Data.CustomPolicies;
+using Data.Entities.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,8 +29,36 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+
+            #region Identity Configuration 
+
+            services.Configure<IdentityOptions>(
+                options =>
+                {
+                    options.User.RequireUniqueEmail = true;
+
+                    options.Password.RequiredLength = 8;
+                    options.Password.RequireLowercase = true;
+                    options.Password.RequireUppercase = true;
+                    options.Password.RequireNonAlphanumeric = true;
+                    options.Password.RequireDigit = true;
+                });
+
+            services.ConfigureApplicationCookie(
+                options =>
+                {
+                    options.AccessDeniedPath = ""; // sonra eklenecek 
+                    options.Cookie.Name = "AspNetCore.Identity.Application";
+                    //options.Cookie.HttpOnly = false; // client-side tarafýndan cookie'ye eriþimi engelleme
+                    //options.Cookie.SecurePolicy = CookieSecurePolicy.Always; // HTTPS üzerinden cookieyi eriþebilir yapma
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+                    options.LoginPath = ""; // sonra eklenecek
+                    options.SlidingExpiration = true;
+                });
+            //services.AddIdentity<User, Role>(options=>options.SignIn.RequireConfirmedEmail=true).AddEntityFrameworkStores(/*DbContext*/).AddPasswordValidator<CustomPasswordPolicy>().AddUserValidator<CustomEmailPolicy>(); 
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +70,8 @@ namespace WebAPI
             }
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
