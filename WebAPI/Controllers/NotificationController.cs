18b7d6dc48adeab23019e1136b7d2c5f96778248
Ayşenur Uğur <aysenurugur98@icloud.com>
2021-10-05 21:18:00 +0300

    promotion and notification controller

---
+++ WebAPI/Controllers/NotificationController.cs
@@ -0,0 +1,59 @@
+﻿
+using AutoMapper;
+using Core.Entities;
+using Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.DTOs;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        INotificationService notificationService;
+        IMapper mapper;
+        public NotificationController(INotificationService notificationService, IMapper mapper)
+        {
+            this.notificationService = notificationService;
+            this.mapper = mapper;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetNotificationsByUserIdAsync(Guid id)
+        {
+            IEnumerable<Notification> notifications = await notificationService.GetNotificationsByUserIdAsync(id);
+            IEnumerable<NotificationDTO> notificationDTOs = mapper.Map<IEnumerable<Notification>, IEnumerable<NotificationDTO>>(notifications);
+            return Ok(notificationDTOs);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetNotificationByIdAsync(Guid id)
+        {
+            Notification notification = await notificationService.GetNotificationByIdAsync(id);
+            NotificationDTO notificationDTO = mapper.Map<Notification, NotificationDTO>(notification);
+            return Ok(notificationDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddNotificationAsync(NotificationDTO notificationDTO)
+        {
+            Notification notification = mapper.Map<NotificationDTO, Notification>(notificationDTO);
+            Notification notificationToBeCreated = await notificationService.CreateNotificationAsync(notification);
+            NotificationDTO createdNotificationDTO = mapper.Map<Notification, NotificationDTO>(notificationToBeCreated);
+            return Ok(createdNotificationDTO);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> ReadNotification(Guid id)
+        {
+            await notificationService.ReadNotificationAsync(id);
+            return Ok();
+        }
+    }
+}
---
+++ WebAPI/DTOs/NotificationDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.DTOs
+{
+    public class NotificationDTO
+    {        
+        public Guid NotificationID { get; set; }
+        public Guid UserID { get; set; }
+        public string Description { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
